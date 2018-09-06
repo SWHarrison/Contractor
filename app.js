2008a76ee9fcad58a8f5a6b45b566272167aba1d
Sam Harrison <samuel.harrison@students.makeschool.com>
2018-09-06 13:12:54 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -19,11 +19,6 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-// OUR MOCK ARRAY OF PROJECTS
-//let reviews = [
-//  { title: "Great Review" },
-//  { title: "Next Review" }
-//]
 
 // INDEX
 app.get('/', (req, res) => {
@@ -42,13 +37,22 @@ app.get('/reviews/new', (req, res) => {
   res.render('reviews-new', {});
 })
 
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
 // CREATE
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
   }).catch((err) => {
-    console.log(err.message);
+    console.log(err.message)
   })
 })
 
