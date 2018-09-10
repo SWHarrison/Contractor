849140a9eea167695b004e3c42cb1f7a4a2dfacb
Sam Harrison <samuel.harrison@students.makeschool.com>
2018-09-10 15:02:39 -0700

    Users can destroy reviews

--- app.js
+++ app.js
@@ -80,6 +80,16 @@ app.get('/reviews/:id/edit', function (req, res) {
   })
 })
 
+// DELETE
+app.delete('/reviews/:id', function (req, res) {
+  console.log("DELETE review")
+  Review.findByIdAndRemove(req.params.id).then((review) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
