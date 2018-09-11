2c2660e9fbc190bc728f92d514292c77251bf6b1
Sam Harrison <samuel.harrison@students.makeschool.com>
2018-09-11 11:43:41 -0700

    Refractored code and started on automatic testing

--- app.js
+++ app.js
@@ -3,15 +3,7 @@ const methodOverride = require('method-override')
 
 const app = express()
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
-
-const Review = mongoose.model('Review', {
-  title: String,
-  description: String,
-  movieTitle: String,
-  rating: Number
-});
+const review = require('./controllers/reviews');
 
 const bodyParser = require('body-parser');
 
@@ -24,71 +16,7 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
-// INDEX
-app.get('/', (req, res) => {
-  Review.find()
-    .then(reviews => {
-      res.render('reviews-index', { reviews: reviews });
-    })
-    .catch(err => {
-      console.log(err);
-    })
-})
-
-
-// NEW
-app.get('/reviews/new', (req, res) => {
-  res.render('reviews-new', {});
-})
-
-// SHOW
-app.get('/reviews/:id', (req, res) => {
-  Review.findById(req.params.id).then((review) => {
-    res.render('reviews-show', { review: review })
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
-
-// CREATE
-app.post('/reviews', (req, res) => {
-  Review.create(req.body).then((review) => {
-    console.log(review)
-    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
-  }).catch((err) => {
-    console.log(err.message)
-  })
-})
-
-// UPDATE
-app.put('/reviews/:id', (req, res) => {
-  Review.findByIdAndUpdate(req.params.id, req.body)
-    .then(review => {
-      res.redirect(`/reviews/${review._id}`)
-    })
-    .catch(err => {
-      console.log(err.message)
-    })
-})
-
-// EDIT
-app.get('/reviews/:id/edit', function (req, res) {
-    console.log('This is the request body' + req.body)
-  Review.findById(req.params.id, function(err, review) {
-    res.render('reviews-edit', {review: review});
-  })
-})
-
-// DELETE
-app.delete('/reviews/:id', function (req, res) {
-  console.log("DELETE review")
-  Review.findByIdAndRemove(req.params.id).then((review) => {
-    res.redirect('/');
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
+review(app)
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
---
+++ models/review.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+
+module.exports = mongoose.model('Review', {
+  title: String,
+  description: String,
+  movieTitle: String,
+  rating: Number
+});
